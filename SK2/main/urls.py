628859d70add5c50a4aaae29e84e9c5ce90d3fbf
krishna3366 <54215170+krishna3366@users.noreply.github.com>
2023-01-03 20:13:46 +0530

    Delete SK2 directory

--- SK2/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from . import views
-
-
-urlpatterns = [
-    path('',views.main, name= "home"),
-    path('enquiry',views.enquiry, name= "enquiry"),
-    path('feedback',views.feedback,name='feedback')
-]
